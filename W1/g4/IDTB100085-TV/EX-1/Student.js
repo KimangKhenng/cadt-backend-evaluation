1bf230b6ab0e4a871fe2454056661e87c62969ee
Tong Vorak <vorak.tong@student.cadt.edu.kh>
2025-05-27 20:32:25 +0700

    Refactored the code based on comment

--- W1/g4/IDTB100085-TV/EX-1/Student.js
+++ W1/g4/IDTB100085-TV/EX-1/Student.js
@@ -1,20 +1,24 @@
 // Import fs for fileSystem
-import fs from 'fs/promises';
+const fs = require('fs');
+const path = require('path');
 
-const filePath = "./hello.txt";
+// use path.resolve to make the path absolute
+const filePath = path.resolve(__dirname, "hello.txt");
 
-async function handleFile() {
-    try{
-        // Write to file (synchronously)
-        await fs.writeFile(filePath, "Hello, Node.js beginner!");
-
-        // Read file (synchronously)
-        const content = await fs.readFile(filePath, "utf8");
-        console.log("File content:", content);
-    }
-    catch (error){
-        console.error("Error", error)
-    }
+// Write to file (synchronously)
+try{
+    fs.writeFileSync(filePath, 'Hello, Node.js beginner!', 'utf-8');
+    console.log('File written successfully!');
+}
+catch (err){
+    console.error('Error writing file', err.message);
 }
 
-handleFile();
+// Read from file (synchronously)
+try{
+    const content = fs.readFileSync(filePath, 'utf-8');
+    console.log('File content: ', content);
+}
+catch (err){
+    console.error('Error reading file', err.message);
+}
