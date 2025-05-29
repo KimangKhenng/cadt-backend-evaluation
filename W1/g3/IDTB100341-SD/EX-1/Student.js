2c77fb4a5a02a8d3c3b3724c5217ff1c90233048
Deny <sokundeny18@gmail.com>
2025-05-29 12:50:04 +0700

    change student.js

--- W1/g3/IDTB100341-SD/EX-1/Student.js
+++ W1/g3/IDTB100341-SD/EX-1/Student.js
@@ -1,16 +1,10 @@
-import fs from 'fs';
-const filePath = "./hello.txt";
-fs.writeFile(filePath, "Hello, Node.js beginner!", (err) => {
-    if (err) {
-      console.error("Error writing to file:", err);
-      return;
-    }
+import fs from 'fs'
 
-    fs.readFile(filePath, "utf8", (err, content) => {
-      if (err) {
-        console.error("Error reading the file:", err);
-        return;
-      }
-      console.log("File content:", content);
-    });
-  });
+const filePath = "./hello.txt";
+
+// Write to a file (synchronously)
+fs.writeFileSync(filePath, "Hello, Node.js beginner!");
+
+// Read the file (synchronously)
+const content = fs.readFileSync(filePath, "utf8");
+console.log("File content:", content);
